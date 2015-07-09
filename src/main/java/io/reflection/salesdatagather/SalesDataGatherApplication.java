71ba2ff482b6697a4245e97c97b2cdca2fcbdd6a
Mitul Amin <mitul@reflection.io>
2015-07-09 16:17:56 +0100

    Added logging configuration to the app

--- src/main/java/io/reflection/salesdatagather/SalesDataGatherApplication.java
+++ src/main/java/io/reflection/salesdatagather/SalesDataGatherApplication.java
@@ -1,15 +1,23 @@
 package io.reflection.salesdatagather;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 public class SalesDataGatherApplication {
+	private transient static final Logger	LOG	= LoggerFactory.getLogger(SalesDataGatherApplication.class.getName());
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(SalesDataGatherApplication.class, args);
+
 		AppConfig appConfig = context.getBean(AppConfig.class);
-		System.out.println("=== Version: " + appConfig.getVersion() + ", Profile: " + appConfig.getProfile());
+		if (LOG.isInfoEnabled()) {
+			LOG.info(String.format("\n\nSales Data Gather App started. Verions: %s under profile: %s\n"
+					+ "=================================================================================\n\n", appConfig.getVersion(), appConfig.getProfile()));
+		}
 	}
 }
