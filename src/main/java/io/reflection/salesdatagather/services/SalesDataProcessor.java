8af662da1a96ee3deae72677c785b9ebbf697cd2
Mitul Amin <mitul@reflection.io>
2015-08-03 16:35:02 +0100

    Lots of protection against NPEs.
    Through out the sales data processing pipeline, we have cases where if a
    sales or downloads or iap sales file is null, the whole thing crashes.
    This commit adds protections against these issues.

--- src/main/java/io/reflection/salesdatagather/services/SalesDataProcessor.java
+++ src/main/java/io/reflection/salesdatagather/services/SalesDataProcessor.java
@@ -26,12 +26,12 @@ import io.reflection.salesdatagather.model.nondb.CsvRevenueAndDownloadEntry;
 public class SalesDataProcessor {
 	private transient static final Logger LOG = LoggerFactory.getLogger(SalesDataProcessor.class.getName());
 
-	public HashMap<Date, CsvRevenueAndDownloadEntry> convertSalesAndDownloadsCSV(Path salesFile, Path downloadsFile, Path iapSalesFile){
+	public HashMap<Date, CsvRevenueAndDownloadEntry> convertSalesAndDownloadsCSV(Path salesFile, Path downloadsFile, Path iapSalesFile) {
 
 		LOG.debug(String.format("Sales file: %s, downloads: %s, iapSalesFile: %s", salesFile, downloadsFile, iapSalesFile));
 
 		HashMap<Date, CsvRevenueAndDownloadEntry> map = getMapOfDatesFromFirstAvailableFile(salesFile, downloadsFile, iapSalesFile);
-		if(map==null) {
+		if (map == null) {
 			LOG.error("Could not process the sales file.");
 			return null;
 		}
@@ -43,110 +43,132 @@ public class SalesDataProcessor {
 	}
 
 	private HashMap<Date, CsvRevenueAndDownloadEntry> getMapOfDatesFromFirstAvailableFile(Path salesFile, Path downloadsFile, Path iapSalesFile) {
-		HashMap<Date, CsvRevenueAndDownloadEntry> map = new HashMap<Date, CsvRevenueAndDownloadEntry>(31);
-		SimpleDateFormat sdf = new SimpleDateFormat("MMM d, yyyy");
+		File fileToProcess = downloadsFile.toFile().exists() ? downloadsFile.toFile() : salesFile.toFile().exists() ? salesFile.toFile() : iapSalesFile.toFile().exists() ? iapSalesFile.toFile() : null;
+		if (fileToProcess == null) return null;
 
-		ArrayList<Date> dateList = new ArrayList<Date>(31);
+		HashMap<Date, CsvRevenueAndDownloadEntry> mapOfDatesFromFile = null;
 
-		File fileToProcess = downloadsFile.toFile().exists()?downloadsFile.toFile():salesFile.toFile().exists()?salesFile.toFile():iapSalesFile.toFile().exists()?iapSalesFile.toFile():null;
-		if(fileToProcess==null) return null;
+		if (downloadsFile != null) {
+			mapOfDatesFromFile = getMapOfDatesFromFile(downloadsFile.toFile());
+		}
+		if (mapOfDatesFromFile == null && salesFile != null) {
+			mapOfDatesFromFile = getMapOfDatesFromFile(salesFile.toFile());
+		}
+		if (mapOfDatesFromFile == null && iapSalesFile != null) {
+			mapOfDatesFromFile = getMapOfDatesFromFile(iapSalesFile.toFile());
+		}
 
-		try (CSVReader csvReader = new CSVReader(new InputStreamReader(new BOMInputStream(new FileInputStream(fileToProcess), false)))){
+		return mapOfDatesFromFile;
+	}
+
+	private HashMap<Date, CsvRevenueAndDownloadEntry> getMapOfDatesFromFile(File fileToProcess) {
+		if (fileToProcess == null || fileToProcess.length() == 0) return null;
+
+		try (CSVReader csvReader = new CSVReader(new InputStreamReader(new BOMInputStream(new FileInputStream(fileToProcess), false)))) {
 			List<String[]> rows = csvReader.readAll();
 
-			if(rows.size()<1) throw new RuntimeException("Not enough records to process in this csv file");
+			if (rows.size() < 1) throw new RuntimeException("Not enough records to process in this csv file");
 
 			List<String> dateRow = Arrays.asList(rows.get(0));
 
 			// we expect the sales file to have platform in cell 0,0 and Measure in the second column of the first row 1,2.
-			if(!"Platform".equals(dateRow.get(0)) || !"Measure".equals(dateRow.get(1))) throw new RuntimeException("File does not start with the data we expected.");
+			if (!"Platform".equals(dateRow.get(0)) || !"Measure".equals(dateRow.get(1))) throw new RuntimeException("File does not start with the data we expected.");
 
-			//we loop through from the third column to the second last (the last one has the total values)
+			HashMap<Date, CsvRevenueAndDownloadEntry> map = new HashMap<Date, CsvRevenueAndDownloadEntry>(31);
+			SimpleDateFormat sdf = new SimpleDateFormat("MMM d, yyyy");
+			ArrayList<Date> dateList = new ArrayList<Date>(31);
+
+			// we loop through from the third column to the second last (the last one has the total values)
 			Date date = null;
-			for(int i=2; i < dateRow.size()-1; i++) {
+			for (int i = 2; i < dateRow.size() - 1; i++) {
 				String dateValue = dateRow.get(i);
 				try {
 					date = sdf.parse(dateValue);
 				} catch (ParseException e) {
-					if(LOG.isDebugEnabled()) {
-						LOG.debug("Could not parse the date on column "+(i+1)+" of the sales file. The value is: "+dateValue, e);
+					if (LOG.isDebugEnabled()) {
+						LOG.debug("Could not parse the date on column " + (i + 1) + " of the sales file. The value is: " + dateValue, e);
 						return null;
 					}
 				}
 
 				dateList.add(date);
-				map.put(date,  new CsvRevenueAndDownloadEntry(date));
+				map.put(date, new CsvRevenueAndDownloadEntry(date));
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
+			return map;
+		} catch (Exception e) {
+			LOG.error("IO exception when trying to read file to generate a map of dates.", e);
 		}
 
-		return map;
+		return null;
 	}
 
 	private void convertSales(HashMap<Date, CsvRevenueAndDownloadEntry> map, Path salesFile) {
-		if(!salesFile.toFile().exists()) return;
+		if (salesFile == null || !salesFile.toFile().exists()) return;
 
 		SimpleDateFormat sdf = new SimpleDateFormat("MMM d, yyyy");
 
 		ArrayList<Date> dateList = new ArrayList<Date>(31);
-		try (CSVReader csvReader = new CSVReader(new InputStreamReader(new BOMInputStream(new FileInputStream(salesFile.toFile()), false)))){
+		try (CSVReader csvReader = new CSVReader(new InputStreamReader(new BOMInputStream(new FileInputStream(salesFile.toFile()), false)))) {
 
 			List<String[]> rows = csvReader.readAll();
 
-			if(rows.size()<1) throw new RuntimeException("Not enough records to process in this csv file");
+			if (rows.size() < 1) throw new RuntimeException("Not enough records to process in this csv file");
 
 			List<String> dateRow = Arrays.asList(rows.get(0));
 
 			// we expect the sales file to have platform in cell 0,0 and Measure in the second column of the first row 1,2.
-			if(!"Platform".equals(dateRow.get(0)) || !"Measure".equals(dateRow.get(1))) throw new RuntimeException("File does not start with the data we expected.");
+			if (!"Platform".equals(dateRow.get(0)) || !"Measure".equals(dateRow.get(1))) throw new RuntimeException("File does not start with the data we expected.");
 
-			//we loop through from the third column to the second last (the last one has the total values)
+			// we loop through from the third column to the second last (the last one has the total values)
 			Date date = null;
-			for(int i=2; i < dateRow.size()-1; i++) {
+			for (int i = 2; i < dateRow.size() - 1; i++) {
 				String dateValue = dateRow.get(i);
 				try {
 					date = sdf.parse(dateValue);
 				} catch (ParseException e) {
-					if(LOG.isDebugEnabled()) {
-						LOG.debug("Could not parse the date on column "+(i+1)+" of the sales file. The value is: "+dateValue, e);
+					if (LOG.isDebugEnabled()) {
+						LOG.debug("Could not parse the date on column " + (i + 1) + " of the sales file. The value is: " + dateValue, e);
 						return;
 					}
 				}
 
 				dateList.add(date);
-				map.put(date,  new CsvRevenueAndDownloadEntry(date));
+				map.put(date, new CsvRevenueAndDownloadEntry(date));
 			}
 
-			//loop through the platforms present
-			for(int currentLine=1;currentLine<rows.size()-1;currentLine++) {
+			// loop through the platforms present
+			for (int currentLine = 1; currentLine < rows.size() - 1; currentLine++) {
 				List<String> row = Arrays.asList(rows.get(currentLine));
 
 				ITunesPlatform platform = null;
 
-				try {platform = ITunesPlatform.getPlatformByName(row.get(0).trim());}catch(Exception e) {LOG.error("***** Exception while trying to get platform for text: "+row.get(0)+e.getMessage());}
+				try {
+					platform = ITunesPlatform.getPlatformByName(row.get(0).trim());
+				} catch (Exception e) {
+					LOG.error("***** Exception while trying to get platform for text: " + row.get(0) + e.getMessage());
+				}
 
 				LOG.debug(String.format("Got platform %s from string %s", platform, row.get(0)));
 
-				//try and get the platform from the string. If there is a problem, proceed to the next row
-				if(platform==null) {
+				// try and get the platform from the string. If there is a problem, proceed to the next row
+				if (platform == null) {
 					continue;
 				}
 
-				for(int i=2; i < row.size()-1; i++) {
+				for (int i = 2; i < row.size() - 1; i++) {
 					int value = 0;
 
 					try {
 						Double saleValue = Double.parseDouble(row.get(i));
 						value = (int) (100 * saleValue);
 
-						LOG.debug(String.format("Processing sale value for row %d. String: %s, Double: %f, Int: %d",i, row.get(i), saleValue, value));
+						LOG.debug(String.format("Processing sale value for row %d. String: %s, Double: %f, Int: %d", i, row.get(i), saleValue, value));
 					} catch (NumberFormatException e) {
-						LOG.debug("Could not convert revenue string into a number. String was: "+row.get(i));
+						LOG.debug("Could not convert revenue string into a number. String was: " + row.get(i));
 					}
 
-					if(value!=0) {
-						map.get(dateList.get(i-2)).setRevenueValue(platform, value);
+					if (value != 0) {
+						map.get(dateList.get(i - 2)).setRevenueValue(platform, value);
 					}
 				}
 			}
@@ -156,29 +178,29 @@ public class SalesDataProcessor {
 	}
 
 	private void convertDownloads(HashMap<Date, CsvRevenueAndDownloadEntry> map, Path downloadsFile) {
-		if(!downloadsFile.toFile().exists()) return;
+		if (downloadsFile == null || !downloadsFile.toFile().exists()) return;
 
 		SimpleDateFormat sdf = new SimpleDateFormat("MMM d, yyyy");
 		ArrayList<Date> dateList = new ArrayList<Date>(31);
-		try (CSVReader csvReader = new CSVReader(new InputStreamReader(new BOMInputStream(new FileInputStream(downloadsFile.toFile()), false)))){
+		try (CSVReader csvReader = new CSVReader(new InputStreamReader(new BOMInputStream(new FileInputStream(downloadsFile.toFile()), false)))) {
 
 			List<String[]> rows = csvReader.readAll();
 
-			if(rows.size()<1) throw new RuntimeException("Not enough records to process in this csv file");
+			if (rows.size() < 1) throw new RuntimeException("Not enough records to process in this csv file");
 
 			List<String> dateRow = Arrays.asList(rows.get(0));
 
 			// we expect the sales file to have platform in cell 0,0 and Measure in the second column of the first row 1,2.
-			if(!"Platform".equals(dateRow.get(0)) || !"Measure".equals(dateRow.get(1))) throw new RuntimeException("File does not start with the data we expected.");
+			if (!"Platform".equals(dateRow.get(0)) || !"Measure".equals(dateRow.get(1))) throw new RuntimeException("File does not start with the data we expected.");
 
 			// we loop through from the third column to the second last (the last one has the total values)
 			Date date = null;
-			for(int i=2; i < dateRow.size()-1; i++) {
+			for (int i = 2; i < dateRow.size() - 1; i++) {
 				try {
 					date = sdf.parse(dateRow.get(i));
 				} catch (ParseException e) {
-					if(LOG.isDebugEnabled()) {
-						LOG.debug("Could not parse the date on column "+(i+1)+" of the sales file. The value is: "+dateRow.get(i), e);
+					if (LOG.isDebugEnabled()) {
+						LOG.debug("Could not parse the date on column " + (i + 1) + " of the sales file. The value is: " + dateRow.get(i), e);
 						return;
 					}
 				}
@@ -186,31 +208,34 @@ public class SalesDataProcessor {
 				dateList.add(date);
 			}
 
-			//loop through the platforms present
-			for(int currentLine=1;currentLine<rows.size()-1;currentLine++) {
+			// loop through the platforms present
+			for (int currentLine = 1; currentLine < rows.size() - 1; currentLine++) {
 				List<String> row = Arrays.asList(rows.get(currentLine));
 
 				ITunesPlatform platform = null;
 
-				try {platform = ITunesPlatform.getPlatformByName(row.get(0).toUpperCase());}catch(Exception e) {}
-				//try and get the platform from the string. If there is a problem, proceed to the next row
-				if(platform==null) {
+				try {
+					platform = ITunesPlatform.getPlatformByName(row.get(0).toUpperCase());
+				} catch (Exception e) {
+				}
+				// try and get the platform from the string. If there is a problem, proceed to the next row
+				if (platform == null) {
 					continue;
 				}
 
-				for(int i=2; i < row.size()-1; i++) {
+				for (int i = 2; i < row.size() - 1; i++) {
 					int value = 0;
 
 					try {
 						value = Integer.parseInt(row.get(i));
-						LOG.debug(String.format("Processing downloads value for row %d. String: %s, Int: %d",i, row.get(i), value));
+						LOG.debug(String.format("Processing downloads value for row %d. String: %s, Int: %d", i, row.get(i), value));
 					} catch (NumberFormatException e) {
-						LOG.debug("Could not convert downloads string into a number. String was: "+row.get(i));
+						LOG.debug("Could not convert downloads string into a number. String was: " + row.get(i));
 					}
 
-					if(value!=0) {
-						//TODO get sometimes get a NPE here. Investigate why
-						map.get(dateList.get(i-2)).setDownloadValue(platform, value);
+					if (value != 0) {
+						// TODO get sometimes get a NPE here. Investigate why
+						map.get(dateList.get(i - 2)).setDownloadValue(platform, value);
 					}
 				}
 			}
@@ -220,29 +245,29 @@ public class SalesDataProcessor {
 	}
 
 	private void convertIaps(HashMap<Date, CsvRevenueAndDownloadEntry> map, Path iapSalesFile) {
-		if(!iapSalesFile.toFile().exists()) return;
+		if (iapSalesFile == null || !iapSalesFile.toFile().exists()) return;
 
 		SimpleDateFormat sdf = new SimpleDateFormat("MMM d, yyyy");
 		ArrayList<Date> dateList = new ArrayList<Date>(31);
-		try (CSVReader csvReader = new CSVReader(new InputStreamReader(new BOMInputStream(new FileInputStream(iapSalesFile.toFile()), false)))){
+		try (CSVReader csvReader = new CSVReader(new InputStreamReader(new BOMInputStream(new FileInputStream(iapSalesFile.toFile()), false)))) {
 
 			List<String[]> rows = csvReader.readAll();
 
-			if(rows.size()<1) throw new RuntimeException("Not enough records to process in this csv file");
+			if (rows.size() < 1) throw new RuntimeException("Not enough records to process in this csv file");
 
 			List<String> dateRow = Arrays.asList(rows.get(0));
 
 			// we expect the sales file to have platform in cell 0,0 and Measure in the second column of the first row 1,2.
-			if(!"Platform".equals(dateRow.get(0)) || !"Measure".equals(dateRow.get(1))) throw new RuntimeException("File does not start with the data we expected.");
+			if (!"Platform".equals(dateRow.get(0)) || !"Measure".equals(dateRow.get(1))) throw new RuntimeException("File does not start with the data we expected.");
 
 			// we loop through from the third column to the second last (the last one has the total values)
 			Date date = null;
-			for(int i=2; i < dateRow.size()-1; i++) {
+			for (int i = 2; i < dateRow.size() - 1; i++) {
 				try {
 					date = sdf.parse(dateRow.get(i));
 				} catch (ParseException e) {
-					if(LOG.isDebugEnabled()) {
-						LOG.debug("Could not parse the date on column "+(i+1)+" of the sales file. The value is: "+dateRow.get(i), e);
+					if (LOG.isDebugEnabled()) {
+						LOG.debug("Could not parse the date on column " + (i + 1) + " of the sales file. The value is: " + dateRow.get(i), e);
 						return;
 					}
 				}
@@ -250,36 +275,39 @@ public class SalesDataProcessor {
 				dateList.add(date);
 			}
 
-
-			//loop through the platforms present
-			for(int currentLine=1;currentLine<rows.size()-1;currentLine++) {
+			// loop through the platforms present
+			for (int currentLine = 1; currentLine < rows.size() - 1; currentLine++) {
 				List<String> row = Arrays.asList(rows.get(currentLine));
 
 				ITunesPlatform platform = null;
 
-				try {platform = ITunesPlatform.getPlatformByName(row.get(0).trim());}catch(Exception e) {LOG.error("***** Exception while trying to get platform for text: "+row.get(0)+e.getMessage());}
+				try {
+					platform = ITunesPlatform.getPlatformByName(row.get(0).trim());
+				} catch (Exception e) {
+					LOG.error("***** Exception while trying to get platform for text: " + row.get(0) + e.getMessage());
+				}
 
 				LOG.debug(String.format("Got platform %s from string %s", platform, row.get(0)));
 
-				//try and get the platform from the string. If there is a problem, proceed to the next row
-				if(platform==null) {
+				// try and get the platform from the string. If there is a problem, proceed to the next row
+				if (platform == null) {
 					continue;
 				}
 
-				for(int i=2; i < row.size()-1; i++) {
+				for (int i = 2; i < row.size() - 1; i++) {
 					int value = 0;
 
 					try {
 						Double saleValue = Double.parseDouble(row.get(i));
 						value = (int) (100 * saleValue);
 
-						LOG.debug(String.format("Processing IAP sale value for row %d. String: %s, Double: %f, Int: %d",i, row.get(i), saleValue, value));
+						LOG.debug(String.format("Processing IAP sale value for row %d. String: %s, Double: %f, Int: %d", i, row.get(i), saleValue, value));
 					} catch (NumberFormatException e) {
-						LOG.debug("Could not convert IAP revenue string into a number. String was: "+row.get(i));
+						LOG.debug("Could not convert IAP revenue string into a number. String was: " + row.get(i));
 					}
 
-					if(value!=0) {
-						map.get(dateList.get(i-2)).setIapRevenueValue(platform, value);
+					if (value != 0) {
+						map.get(dateList.get(i - 2)).setIapRevenueValue(platform, value);
 					}
 				}
 			}
