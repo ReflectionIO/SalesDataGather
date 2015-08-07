8ca4898252aac777b1cd7e28f9d0c201939186e9
Mitul Amin <mitul@reflection.io>
2015-08-07 14:09:39 +0100

    Split Data uses total revenue instead of universal revenue.

--- src/main/java/io/reflection/salesdatagather/model/repositories/SaleSummaryRepo.java
+++ src/main/java/io/reflection/salesdatagather/model/repositories/SaleSummaryRepo.java
@@ -18,15 +18,14 @@ public class SaleSummaryRepo {
 	private transient static final Logger LOG = LoggerFactory.getLogger(SaleSummaryRepo.class.getName());
 
 	@Autowired
-	private JdbcTemplate	jdbcTemplate;
-
+	private JdbcTemplate jdbcTemplate;
 
 	public int updateSalesSummaries(DataAccount dataAccount, String countryCode, String mainItemId, String itemTitle, Date date, double phoneRevenueRatio, double tabletRevenueRatio,
 			double phoneIapRevenueRatio, double tabletIapRevenueRatio, int phoneDownloads, int tabletDownloads, int totalDownloads) {
 
-		String update="update sale_summary set " +
-				"	iphone_app_revenue = ROUND(universal_app_revenue*?), " +
-				"    ipad_app_revenue = ROUND(universal_app_revenue*?), " +
+		String update = "update sale_summary set " +
+				"	iphone_app_revenue = ROUND(total_app_revenue*?), " +
+				"    ipad_app_revenue = ROUND(total_app_revenue*?), " +
 				"    iphone_iap_revenue = ROUND(iap_revenue*?), " +
 				"    ipad_iap_revenue = ROUND(iap_revenue*?), " +
 				"    iphone_downloads=?," +
@@ -42,7 +41,7 @@ public class SaleSummaryRepo {
 
 			@Override
 			public void setValues(PreparedStatement ps) throws SQLException {
-				int paramCount=1;
+				int paramCount = 1;
 				java.sql.Date sqlDate = new java.sql.Date(date.getTime());
 
 				LOG.debug(String.format("update %d, %s, %s, %s, %s, %f, %f, %f, %f, %d, %d, %d", dataAccount.getId(), sqlDate, mainItemId, itemTitle, countryCode,
@@ -63,7 +62,7 @@ public class SaleSummaryRepo {
 			}
 		});
 
-		if(updatedRowCount==0) {
+		if (updatedRowCount == 0) {
 			LOG.info(String.format("Could not update sale summary record for: dataaccount: %d, itemid: %s, date: %s, country: %s", dataAccount.getId(), mainItemId, date, countryCode));
 		}
 
